e90756c59e3bba7f5375fc41e8a5b5e860d8420a
kuu <akumakumu@gmail.com>
2024-07-29 00:22:37 +0700

    adding dotenv config

--- databases/db.go
+++ databases/db.go
@@ -1,12 +1,28 @@
 package databases
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDatabase() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db_host := os.Getenv("DATABASE_HOST")
+	db_port := os.Getenv("DATABASE_PORT")
+	db_user := os.Getenv("DATABASE_USERNAME")
+	db_pass := os.Getenv("DATABASE_PASSWORD")
+	db_name := os.Getenv("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", db_host, db_user, db_pass, db_name, db_port)
 
 	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
